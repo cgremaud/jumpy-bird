d2c560b64e1da7bc48feac4e62782398b6a7a6eb
C. Gremaud <colyn.gremaud@gmail.com>
2021-10-09 10:10:10 -0500

    player now jumps higher longer space is held

--- PlayerController.cs
+++ PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     public float deathThreshhold = -7.5f;
     private int comboCount = 1;
     public ParticleSystem killParticle;
+    public bool jumpKeyHeld = false;
    
     // Start is called before the first frame update
     void Start()
@@ -34,67 +35,97 @@ public class PlayerController : MonoBehaviour
         if (gameManager.isGameActive)
         {
             //Move left/right when left/right arrow is pressed
-            //MovePlayer();
-            if ((playerRb.velocity.x < maxSpeed & playerRb.velocity.x > -maxSpeed) & isOnGround)
-            {
-                playerRb.AddForce(Vector2.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime, ForceMode2D.Impulse);
-            }
-            else /*if ((playerRb.velocity.x < maxSpeed & playerRb.velocity.x > -maxSpeed) & !isOnGround) */
-            {
-                playerRb.AddForce(Vector2.right * Input.GetAxis("Horizontal") * maneuverability * Time.deltaTime, ForceMode2D.Impulse);
-                //push back if going greater than maxSpeed
-                if (playerRb.velocity.x > maxSpeed)
-                {
-                    Vector2 pushBack = new Vector2(5f, 0);
-                    playerRb.AddForce(Vector2.right * -pushBack, ForceMode2D.Force);
-                }
-                else if (playerRb.velocity.x < -maxSpeed)
-                {
-                    Vector2 pushBack = new Vector2(5f, 0);
-                    playerRb.AddForce(Vector2.right * pushBack, ForceMode2D.Force);
-                }
-            }
-            //handle rotation and animation states.
-            //RotateAnimPlayer();
-            if (playerRb.velocity.x > 0)
-            {
-                if (gameObject.transform.rotation.y != 0)
-                {
-                    gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
-                }
-                playerAnim.SetBool("running_b", true);
-            }
-            else if (playerRb.velocity.x < 0)
-            {
-                if (gameObject.transform.rotation.y == 0)
-                {
-                    gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
-                }
-                playerAnim.SetBool("running_b", true);
-            }
-            else
-            {
-                playerAnim.SetBool("running_b", false);
+            MovePlayer();
 
-            }
+            //handle rotation and animation states.
+            RotateAnimPlayer();
+
             //Jump
-            //Jump();
-            if (Input.GetKeyDown(KeyCode.Space) & (isOnGround | touchedEnemy))
+            Jump();
+
+            //Exert downward force unless jump button is held
+            if (!isOnGround)
             {
-                playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                isOnGround = false;
-                touchedEnemy = false;
-                playerAnim.SetTrigger("jump_t");
+                if (!jumpKeyHeld && Vector2.Dot(playerRb.velocity, Vector2.up) > 0)
+                {
+                    playerRb.AddForce(Vector2.down * jumpForce * 50 * Time.deltaTime, ForceMode2D.Force);
+                }
             }
+
+
 
             //Check for death
-            if (playerRb.position.y < deathThreshhold)
+            CheckDeath();
+
+        }
+    }
+
+    private void MovePlayer()
+    {
+        if ((playerRb.velocity.x < maxSpeed & playerRb.velocity.x > -maxSpeed) & isOnGround)
+        {
+            playerRb.AddForce(Vector2.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime, ForceMode2D.Impulse);
+        }
+        else /*if ((playerRb.velocity.x < maxSpeed & playerRb.velocity.x > -maxSpeed) & !isOnGround) */
+        {
+            playerRb.AddForce(Vector2.right * Input.GetAxis("Horizontal") * maneuverability * Time.deltaTime, ForceMode2D.Impulse);
+            //push back if going greater than maxSpeed
+            if (playerRb.velocity.x > maxSpeed)
             {
-                gameManager.GameOver();
-                Destroy(gameObject);
+                Vector2 pushBack = new Vector2(10f, 0);
+                playerRb.AddForce(Vector2.right * -pushBack, ForceMode2D.Force);
+            }
+            else if (playerRb.velocity.x < -maxSpeed)
+            {
+                Vector2 pushBack = new Vector2(10f, 0);
+                playerRb.AddForce(Vector2.right * pushBack, ForceMode2D.Force);
             }
         }
-        
+    }
+    private void CheckDeath()
+    {
+        if (playerRb.position.y < deathThreshhold)
+        {
+            gameManager.GameOver();
+            Destroy(gameObject);
+        }
+    }
+    private void RotateAnimPlayer()
+    {
+        if (playerRb.velocity.x > 0)
+        {
+            if (gameObject.transform.rotation.y != 0)
+            {
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            playerAnim.SetBool("running_b", true);
+        }
+        else if (playerRb.velocity.x < 0)
+        {
+            if (gameObject.transform.rotation.y == 0)
+            {
+                gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            playerAnim.SetBool("running_b", true);
+        }
+        else
+        {
+            playerAnim.SetBool("running_b", false);
+        }
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) & (isOnGround | touchedEnemy))
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+            touchedEnemy = false;
+            playerAnim.SetTrigger("jump_t");
+            jumpKeyHeld = true;
+        } else if (Input.GetKeyUp(KeyCode.Space) & !isOnGround)
+        {
+            jumpKeyHeld = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -108,9 +139,9 @@ public class PlayerController : MonoBehaviour
             //Debug.Log("Touch Grass");
         } 
         //Handle collision w/ enemy
-        if (collision.collider.GetType() == typeof(BoxCollider2D) & collision.gameObject.CompareTag("EnemyHurtbox") & playerRb.velocity.y <= 0.2)
+        if (collision.collider.GetType() == typeof(BoxCollider2D) & collision.gameObject.CompareTag("EnemyHurtbox") & playerRb.velocity.y <= 0)
         {
-            gameManager.UpdateScore(10 * comboCount);
+            gameManager.UpdateScore(10 * comboCount); //TODO replace 10 with collision.gameObject.scoreVal
             if (!isOnGround & touchedEnemy)
             {
                 comboCount += 1;
